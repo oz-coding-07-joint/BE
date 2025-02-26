f946c5723229157fb4ea1aa97e7b99eb05f4f5d9
Umdoong <umdoongs@gmail.com>
2025-02-26 18:16:21 +0900

    :sparkles: user에 있던 terms 분리

--- apps/users/models.py
+++ apps/users/models.py
@@ -59,32 +59,6 @@ class User(BaseModel, AbstractBaseUser, PermissionsMixin, SoftDeleteModel):
         db_table = "user"
 
 
-# 사용자의 약관 동의 정보를 저장하는 모델
-class TermsAgreement(BaseModel):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    terms = models.ForeignKey("Terms", on_delete=models.CASCADE)
-    is_active = models.BooleanField(default=True)
-
-    class Meta:
-        db_table = "terms_agreement"
-
-
-# 약관 정보를 저장하는 모델
-class Terms(BaseModel):
-    name = models.CharField(max_length=50)
-    detail = models.TextField()
-    is_active = models.BooleanField(default=True)
-    is_required = models.BooleanField(default=True)
-
-    class Meta:
-        db_table = "terms"
-        verbose_name = "Terms"
-        verbose_name_plural = "Terms"
-
-    def __str__(self):
-        return f"{self.name}"
-
-
 class Student(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
