02ea5601eebb6a41a3c704043ed7ed047c498ed8
Umdoong <umdoongs@gmail.com>
2025-02-26 18:42:17 +0900

    :pencil2: run black

--- apps/terms/admin.py
+++ apps/terms/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Terms, TermsAgreement
+
 from ..common.admin import BaseModelAdmin
+from .models import Terms, TermsAgreement
 
 
 # Register your models here.
