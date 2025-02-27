83f5d930ca327be8c61d7dcfc5094b8c60462ec0
Umdoong <umdoongs@gmail.com>
2025-02-27 11:13:46 +0900

    :bulb: 학생이 회원탈퇴해도 리뷰는 남아있도록 구조 변경

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -54,7 +54,8 @@ class Enrollment(BaseModel):
 
 class Review(BaseModel):
     lecture = models.ForeignKey(Lecture, on_delete=models.CASCADE)
-    student = models.ForeignKey(User, on_delete=models.CASCADE)
+    student = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    student_nickname = models.CharField(max_length=20)
     star = models.DecimalField(max_digits=5, decimal_places=1)
     content = models.CharField(max_length=200)
 
