3eb154df2aa0bd9e6b0164e74dbf48d5a3523511
Jung <jjlee030415@gmail.com>
2025-03-04 18:38:51 +0900

    :pencil2: in_progress에서 _를 -으로 변경

--- apps/registrations/urls.py
+++ apps/registrations/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     # 수강 신청
     path("enrollment/<class_id>/", EnrollmentRegistrationView.as_view(), name="player-list"),
     # 수강중인 수업 조회
-    path("enrollment/in_progress", EnrollmentInProgressView.as_view(), name="player-detail"),
+    path("enrollment/in-progress", EnrollmentInProgressView.as_view(), name="player-detail"),
 ]
