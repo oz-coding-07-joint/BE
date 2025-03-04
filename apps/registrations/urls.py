8b73754e1835ac1a0000804088c299bf5a751b3b
Jung <jjlee030415@gmail.com>
2025-03-04 20:49:57 +0900

    :pencil2: int추가

--- apps/registrations/urls.py
+++ apps/registrations/urls.py
@@ -7,7 +7,7 @@ from .views import (
 
 urlpatterns = [
     # 수강 신청
-    path("enrollment/<class_id>/", EnrollmentRegistrationView.as_view(), name="player-list"),
+    path("enrollment/<int:class_id>/", EnrollmentRegistrationView.as_view(), name="enrollment-create"),
     # 수강중인 수업 조회
-    path("enrollment/in-progress", EnrollmentInProgressView.as_view(), name="player-detail"),
+    path("enrollment/in-progress", EnrollmentInProgressView.as_view(), name="enrollment-in-progress"),
 ]
