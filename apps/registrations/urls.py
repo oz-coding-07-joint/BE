d765368c6b777de87a7a5158b095dc3854cde935
Jung <jjlee030415@gmail.com>
2025-03-04 18:21:39 +0900

    :sparkles: registrations urs 추가

---
+++ apps/registrations/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from .views import (
+    EnrollmentInProgressView,
+    EnrollmentRegistrationView,
+)
+
+urlpatterns = [
+    # 수강 신청
+    path("enrollment/<class_id>/", EnrollmentRegistrationView.as_view(), name="player-list"),
+    # 수강중인 수업 조회
+    path("enrollment/in_progress", EnrollmentInProgressView.as_view(), name="player-detail"),
+]
