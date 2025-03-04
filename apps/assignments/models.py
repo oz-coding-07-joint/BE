fc33dea0f16c7e3abf378ad85a896315483ccb47
Umdoong <umdoongs@gmail.com>
2025-03-04 14:30:09 +0900

    :pencil2: course_material, assignment_interaction -> chapter_video, assignment_comment / file_url을 filefield로 변경

--- apps/assignments/models.py
+++ apps/assignments/models.py
@@ -1,28 +1,28 @@
 from django.db import models
 
 from apps.common.models import BaseModel
-from apps.courses.models import CourseMaterial
+from apps.courses.models import ChapterVideo
 from apps.users.models import User
+from apps.common.utils import assignment_material_path, assignment_comment_file_path
 
 
 class Assignment(BaseModel):
-    course_material = models.ForeignKey(CourseMaterial, on_delete=models.CASCADE)
+    chapter_video = models.ForeignKey(ChapterVideo, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     content = models.CharField(max_length=1000)
-    file_url = models.CharField(max_length=255)
+    file_url = models.FileField(upload_to=assignment_material_path, null=True, blank=True)
 
     class Meta:
         db_table = "assignment"
 
 
-class AssignmentInteraction(BaseModel):
+class AssignmentComment(BaseModel):
     assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     parent_id = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')
     title = models.CharField(max_length=50)
-    file_url = models.CharField(max_length=255)
+    file_url = models.FileField(upload_to=assignment_comment_file_path, null=True, blank=True)
     content = models.CharField(max_length=500)
-    status = models.BooleanField(default=True)
 
     class Meta:
-        db_table = "homework"
+        db_table = "assignment_comment"
