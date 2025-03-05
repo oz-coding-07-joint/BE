3880e1d1692a98f8d3c13cad5d04de1bd4cd9adf
Umdoong <umdoongs@gmail.com>
2025-03-05 11:57:42 +0900

    :sparkles: terms, users views.py extend_schema 생성

--- apps/terms/views.py
+++ apps/terms/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from drf_spectacular.utils import extend_schema
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .serializers import TermsSerializer
+
+
+class TermsView(APIView):
+
+    @extend_schema(
+        summary="약관 조회", description="약관의 내용을 확인할 수 있습니다", request=TermsSerializer, tags=["Terms"]
+    )
+    def get(self, request):
+        return
