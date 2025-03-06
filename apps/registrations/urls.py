0c07486353af46f4884664c15673c28952a10839
Jung <jjlee030415@gmail.com>
2025-03-06 18:22:08 +0900

    :bug: /추가

--- apps/registrations/urls.py
+++ apps/registrations/urls.py
@@ -7,7 +7,7 @@ from .views import (
 
 urlpatterns = [
     # 수강 신청
-    path("enrollment/<int:class_id>/", EnrollmentRegistrationView.as_view(), name="enrollment-create"),
+    path("enrollment/<int:course_id>/", EnrollmentRegistrationView.as_view(), name="enrollment-create"),
     # 수강중인 수업 조회
-    path("enrollment/in-progress", EnrollmentInProgressView.as_view(), name="enrollment-in-progress"),
+    path("enrollment/in-progress/", EnrollmentInProgressView.as_view(), name="enrollment-in-progress"),
 ]
