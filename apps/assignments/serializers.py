7814f05534a972a8ed317a933ed141e3eb61dc7f
Jung <jjlee030415@gmail.com>
2025-03-07 11:21:06 +0900

    :wrench: 수강생 과제 및 피드백 목록 조회할때 누가 작성 했는지 쉽게 알기위해 nickname추가

--- apps/assignments/serializers.py
+++ apps/assignments/serializers.py
@@ -13,10 +13,11 @@ class AssignmentSerializer(serializers.ModelSerializer):
 # 강의 과제 제출, 수강생 과제 및 피드백 목록 조회
 class AssignmentCommentSerializer(serializers.ModelSerializer):
     replies = serializers.SerializerMethodField()
+    nickname = serializers.CharField(source="user.nickname", read_only=True)
 
     class Meta:
         model = AssignmentComment
-        fields = ["id", "user", "assignment", "parent", "file_url", "content", "created_at", "replies"]
+        fields = ["id", "user", "nickname", "assignment", "parent", "file_url", "content", "created_at", "replies"]
 
     def get_replies(self, obj):
         qs = obj.replies.all()
