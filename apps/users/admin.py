78929027fd57a79972a674eaf5d87df6baae9070
Umdoong <umdoongs@gmail.com>
2025-03-07 13:58:48 +0900

    :pencil2: run black

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -24,15 +24,15 @@ class UserAdmin(BaseModelAdmin):
             form.base_fields["is_superuser"].disabled = True
             form.base_fields["is_staff"].disabled = True
         return form
-    
+
     def get_readonly_fields(self, request, obj=None):
         """
         마지막 superuser가 존재하면 해당 필드를 읽기 전용으로 설정
         """
         readonly_fields = super().get_readonly_fields(request, obj)
         if obj and obj.is_superuser and User.objects.filter(is_superuser=True).count() == 1:
-            readonly_fields = readonly_fields + ('is_superuser',)
-        
+            readonly_fields = readonly_fields + ("is_superuser",)
+
         return readonly_fields
 
     def save_model(self, request, obj, form, change):
@@ -48,4 +48,3 @@ class UserAdmin(BaseModelAdmin):
             obj.password = make_password(obj.password)
 
         super().save_model(request, obj, form, change)
-
