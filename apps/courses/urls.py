3c000957130a865d90e42d92d9b038262ef74765
yerin1562 <yerin1562@gmail.com>
2025-03-12 15:17:40 +0900

    :zap: : progress create added

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -7,6 +7,15 @@ urlpatterns = [
     path("lecture/<lecture_id>/", views.LectureDetailView.as_view()),
     path("lecture_chapter/<lecture_id>/", views.LectureChapterListView.as_view()),
     path("chapter_video/<chapter_video_id>/state/", views.ChapterVideoProgressView.as_view()),
-    path("chapter_video/<chapter_video_id>/progress/", views.ChapterVideoProgressUpdateView.as_view()),
+    path(
+        "chapter_video/<chapter_video_id>/progress/",
+        views.ChapterVideoProgressCreateView.as_view(),
+        name="chapter-video-progress-create",
+    ),
+    path(
+        "chapter_video/<chapter_video_id>/progress/update/",
+        views.ChapterVideoProgressUpdateView.as_view(),
+        name="chapter-video-progress-update",
+    ),
     path("chapter_video/<chapter_video_id>/", views.ChapterVideoDetailView.as_view()),
 ]
