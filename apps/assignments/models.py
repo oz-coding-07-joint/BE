bc6a947d49caabb1d5c555399baada3a3edb8e85
Jung <jjlee030415@gmail.com>
2025-03-14 16:15:53 +0900

    :sparkles: 데이터베이스 삭제 시 NCP Object Storage 파일 자동 삭제 기능 추가

--- apps/assignments/models.py
+++ apps/assignments/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 from apps.common.models import BaseModel
-from apps.common.utils import assignment_comment_file_path, assignment_material_path
+from apps.common.utils import (
+    assignment_comment_file_path,
+    assignment_material_path,
+    delete_file_from_ncp,
+)
 from apps.courses.models import ChapterVideo
 from apps.users.models import User
 
@@ -22,6 +26,12 @@ class Assignment(BaseModel):
         else:
             super().save(*args, **kwargs)
 
+    def delete(self, *args, **kwargs):
+        # NCP Object Storage에서도 파일 삭제
+        if self.file_url:
+            delete_file_from_ncp(self.file_url.name)
+        super().delete(*args, **kwargs)
+
     class Meta:
         db_table = "assignment"
 
@@ -32,6 +42,12 @@ class AssignmentComment(BaseModel):
     parent = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True, related_name="replies")
     file_url = models.FileField(upload_to=assignment_comment_file_path, null=True, blank=True)
     content = models.CharField(max_length=500)
+
+    def delete(self, *args, **kwargs):
+        # NCP Object Storage에서도 파일 삭제
+        if self.file_url:
+            delete_file_from_ncp(self.file_url.name)
+        super().delete(*args, **kwargs)
 
     class Meta:
         db_table = "assignment_comment"
