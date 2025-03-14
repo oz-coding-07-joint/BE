a22728f2fe635d328e38f64a6e67d7e5cff94d1e
yerin1562 <yerin1562@gmail.com>
2025-03-14 11:45:31 +0900

    :art: post progress API added

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path("lecture/", views.LectureListView.as_view(), name="lecture"),
     path("lecture/<lecture_id>/", views.LectureDetailView.as_view()),
     path("lecture_chapter/<lecture_id>/", views.LectureChapterListView.as_view()),
-    path("chapter_video/<chapter_video_id>/state/", views.ChapterVideoProgressView.as_view()),
     path(
         "chapter_video/<chapter_video_id>/progress/",
         views.ChapterVideoProgressCreateView.as_view(),
