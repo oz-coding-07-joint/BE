2ba939ce40c1e7cc90bbbcf8aec1987da304c151
yerin1562 <yerin1562@gmail.com>
2025-03-14 15:00:57 +0900

    :art: run black

--- apps/common/permissions.py
+++ apps/common/permissions.py
@@ -1,5 +1,5 @@
-from rest_framework.permissions import BasePermission
 from rest_framework.exceptions import PermissionDenied
+from rest_framework.permissions import BasePermission
 
 from apps.registrations.models import Enrollment
 from apps.users.models import User
