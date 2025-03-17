35aeb912682edb40f7c8dd3bd6889cbbbbd644f7
Umdoong <umdoongs@gmail.com>
2025-03-17 10:53:28 +0900

    :sparkles: access token을 cookie로 보내던 요청 다시 원상복귀

--- config/settings/base.py
+++ config/settings/base.py
@@ -165,10 +165,7 @@ AUTH_USER_MODEL = "users.User"
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
-    "DEFAULT_AUTHENTICATION_CLASSES": (
-        "config.authentication.CustomJWTAuthentication",
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ),
+    "DEFAULT_AUTHENTICATION_CLASSES": ("rest_framework_simplejwt.authentication.JWTAuthentication",),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
