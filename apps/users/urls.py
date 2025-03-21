a7652a8a0c5ca53181591d65bac410777d8d9dfe
Umdoong <umdoongs@gmail.com>
2025-03-21 13:52:40 +0900

    :pencil2: url 이름 직관적이게 변경

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -19,11 +19,11 @@ urlpatterns = [
     path("verify-email-code/", VerifyEmailCodeView.as_view(), name="verify-email-code"),
     path("signup/", SignUpView.as_view(), name="signup"),
     path("login/", LoginView.as_view(), name="login"),
-    path("kakao-signup-complete/", SocialSignupCompleteView.as_view(), name="social-signup-complete"),
+    path("social-signup-complete/", SocialSignupCompleteView.as_view(), name="social-signup-complete"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("withdrawal/", WithdrawalView.as_view(), name="withdrawal"),
     path("myinfo/", MyinfoView.as_view(), name="myinfo"),
     path("password-change/", ChangePasswordView.as_view(), name="change-password"),
     path("token-refresh/", TokenRefreshView.as_view(), name="token-refresh"),
-    path("social-login/", KakaoAuthView.as_view(), name="social-login"),
+    path("kakao-auth/", KakaoAuthView.as_view(), name="kakao-auth"),
 ]
