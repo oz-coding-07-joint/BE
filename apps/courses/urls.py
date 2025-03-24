a1167cefed21f4b1768f48dd9d91ae9dc4d0e360
yerin1562 <yerin1562@gmail.com>
2025-03-24 19:06:42 +0900

    :memo: int: 몰랐다모 ㅠㅠ

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -4,22 +4,22 @@ from apps.courses import views
 
 urlpatterns = [
     path("lecture/", views.LectureListView.as_view(), name="lecture"),
-    path("lecture/<lecture_id>/", views.LectureDetailView.as_view()),
-    path("lecture_chapter/<lecture_id>/", views.LectureChapterListView.as_view()),
+    path("lecture/<int:lecture_id>/", views.LectureDetailView.as_view()),
+    path("lecture_chapter/<int:lecture_id>/", views.LectureChapterListView.as_view()),
     path(
-        "chapter_video/<chapter_video_id>/state/",
+        "chapter_video/<int:chapter_video_id>/state/",
         views.ChapterVideoProgressRetrieveView.as_view(),
         name="chapter_video_state",
     ),
     path(
-        "chapter_video/<chapter_video_id>/progress/",
+        "chapter_video/<int:chapter_video_id>/progress/",
         views.ChapterVideoProgressCreateView.as_view(),
         name="chapter-video-progress-create",
     ),
     path(
-        "chapter_video/<chapter_video_id>/progress/update/",
+        "chapter_video/<int:chapter_video_id>/progress/update/",
         views.ChapterVideoProgressUpdateView.as_view(),
         name="chapter-video-progress-update",
     ),
-    path("chapter_video/<chapter_video_id>/", views.ChapterVideoDetailView.as_view()),
+    path("chapter_video/<int:chapter_video_id>/", views.ChapterVideoDetailView.as_view()),
 ]
