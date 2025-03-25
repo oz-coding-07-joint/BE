6bd66d26d140758b16d4fde7be035b9b37599dae
Umdoong <umdoongs@gmail.com>
2025-03-26 02:30:40 +0900

    :pencil2: run black

--- apps/users/signals.py
+++ apps/users/signals.py
@@ -1,10 +1,11 @@
-from django.db.models.signals import pre_delete, post_delete
+from django.db.models.signals import post_delete, pre_delete
 from django.dispatch import receiver
 
+from apps.courses.models import Lecture, ProgressTracking
 from apps.registrations.models import Enrollment
 from apps.reviews.models import Review
-from apps.courses.models import Lecture, ProgressTracking
 from apps.users.models import User
+
 
 @receiver(pre_delete, sender=User)
 def delete_related_table(sender, instance, **kwargs):
