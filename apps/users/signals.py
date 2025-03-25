dce567f3d56bd3000de7d2a726cb4b78161b22a1
Umdoong <umdoongs@gmail.com>
2025-03-26 02:26:16 +0900

    :bug: one to one table과 연관된 테이블에 row가 있으면 user의 회원탈퇴가 안되는 버그 해결

---
+++ apps/users/signals.py
@@ -0,0 +1,15 @@
+from django.db.models.signals import pre_delete, post_delete
+from django.dispatch import receiver
+
+from apps.registrations.models import Enrollment
+from apps.reviews.models import Review
+from apps.courses.models import Lecture, ProgressTracking
+from apps.users.models import User
+
+@receiver(pre_delete, sender=User)
+def delete_related_table(sender, instance, **kwargs):
+    # user 삭제 전에 one to one table과 관련된 ProgressTracking 삭제
+    ProgressTracking.objects.filter(student__user=instance).delete()
+    Enrollment.objects.filter(student__user=instance).delete()
+    Review.objects.filter(student__user=instance).delete()
+    Lecture.objects.filter(student__user=instance).delete()
