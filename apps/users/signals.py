88479256dc1efe981cc56ed4be0c0d2d28ac3681
Umdoong <umdoongs@gmail.com>
2025-03-26 10:56:12 +0900

    :bug: 회원탈퇴 시그널에서 유저가 학생인 경우와 강사인 경우를 분리함

--- apps/users/signals.py
+++ apps/users/signals.py
@@ -9,8 +9,12 @@ from apps.users.models import User
 
 @receiver(pre_delete, sender=User)
 def delete_related_table(sender, instance, **kwargs):
-    # user 삭제 전에 one to one table과 관련된 ProgressTracking 삭제
-    ProgressTracking.objects.filter(student__user=instance).delete()
-    Enrollment.objects.filter(student__user=instance).delete()
-    Review.objects.filter(student__user=instance).delete()
-    Lecture.objects.filter(student__user=instance).delete()
+    # 유저가 학생(Student)인 경우
+    if hasattr(instance, "student"):
+        ProgressTracking.objects.filter(student=instance.student).delete()
+        Enrollment.objects.filter(student=instance.student).delete()
+        Review.objects.filter(student=instance.student).delete()
+    
+    # 유저가 강사(Instructor)인 경우
+    if hasattr(instance, "instructor"):
+        Lecture.objects.filter(instructor=instance.instructor).delete()
