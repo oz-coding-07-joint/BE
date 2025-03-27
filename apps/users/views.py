cd626970ce3f2650ad54355ca39ea362f9ceedad
Umdoong <umdoongs@gmail.com>
2025-03-27 13:45:46 +0900

    :sparkles: 소셜로그인 추가 정보 입력 받을 때도 예외처리 추가

--- apps/users/views.py
+++ apps/users/views.py
@@ -750,6 +750,14 @@ class SocialSignupCompleteView(APIView):
                         if not Student.objects.filter(user=user).exists():
                             Student.objects.create(user=user)
                     return Response({"detail": "추가 정보 입력 완료!"}, status=status.HTTP_200_OK)
+                
+                except serializers.ValidationError as e:
+                    return Response(e.detail, status=status.HTTP_400_BAD_REQUEST)
+
+                except IntegrityError:
+                    return Response(
+                        {"error": "이미 사용 중인 닉네임 또는 전화번호 입니다."}, status=status.HTTP_400_BAD_REQUEST
+                    )
 
                 except Exception:
                     return Response(
