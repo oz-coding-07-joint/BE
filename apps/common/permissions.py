b7b3ce8d0e7b4380e904fa528d8dbb95cead9f97
yerin1562 <yerin1562@gmail.com>
2025-03-28 14:10:00 +0900

    :art: instructor permission added

--- apps/common/permissions.py
+++ apps/common/permissions.py
@@ -1,32 +1,6 @@
-from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import BasePermission
 
 from apps.registrations.models import Enrollment
-from apps.users.models import User
-
-
-class IsEnrolledStudent(BasePermission):
-    """
-    로그인한 사용자 중 수강신청이 승인된 학생만 접근 가능.
-    """
-
-    def has_permission(self, request, view):
-        # 사용자 인증 확인
-        if not request.user or not request.user.is_authenticated:
-            raise PermissionDenied("로그인이 필요한 서비스 입니다.")
-
-        user = User.objects.select_related("student").filter(id=request.user.id).first()
-
-        if not (user and user.student):  # 한 줄로 합쳐서 최적화
-            raise PermissionDenied("학생만 접근할 수 있습니다.")
-
-        student = user.student  # student 객체 안전하게 사용 가능
-
-        # 수강 신청 여부 확인
-        if not Enrollment.objects.filter(student=student, is_active=True).exists():
-            raise PermissionDenied("해당 강의를 수강 중인 학생만 접근할 수 있습니다.")  # 403 Forbidden 발생
-
-        return True  # 모든 조건을 만족하면 접근 허용
 
 
 class IsActiveStudentOrInstructor(BasePermission):
